e15bccd65cae965ebba9b585a604bee53a56d37c
Andy VanEe <1andyvanee@gmail.com>
2023-07-08 21:14:00 -0600

    lint with prettier

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ This is a small experiment in the techniques [mentioned here](https://dev.to/tig
 I'm sure there are downsides to rendering a website this way, but it works!
 
 1. In [handlers/home.js](https://github.com/andyvanee/streaming-html/blob/main/handlers/home.js#L33-L37), write the main part of the HTML and leave the connection open
-2.  After random timeouts (simulating async backend processing)
-  - write HTML template fragment to the response
-  - write script to response which replaces that part of the body
+2. After random timeouts (simulating async backend processing)
+    - write HTML template fragment to the response
+    - write script to response which replaces that part of the body
 3. Once all fragments are written, call `res.end()`
 
 My intuition was that the page would be non-interactive until the end of step 3, but this is not the case. Although `DOMContentLoaded` waits
--- handlers/home.js
+++ handlers/home.js
@@ -1,47 +1,50 @@
-import { readFile } from "node:fs/promises";
+import { readFile } from 'node:fs/promises'
 
 const deferredHtml = async (selector, filename) => {
-  const contents = await readFile(filename, { encoding: "utf-8" });
-  return `
-    <div id="__${selector}__loader__" style="display:none">
-        <template id="${selector}-fragment">${contents}</template>
-        <script>
-            ;(() => {
-                const container = document.querySelector(".${selector}")
-                const fragment = document.querySelector("#${selector}-fragment")
-                const shadow = container.attachShadow({mode: "open"})
-                shadow.appendChild(fragment.content.cloneNode(true))
-                container.classList.remove("loading")
-                fragment.parentElement.remove()
-            })();
-        </script>
-    </div>
-    `;
-};
+    const contents = await readFile(filename, { encoding: 'utf-8' })
+    return `
+        <div id="__${selector}__loader__" style="display:none">
+            <template id="${selector}-fragment">${contents}</template>
+            <script>
+                ;(() => {
+                    const container = document.querySelector(".${selector}")
+                    const fragment = document.querySelector("#${selector}-fragment")
+                    const shadow = container.attachShadow({mode: "open"})
+                    shadow.appendChild(fragment.content.cloneNode(true))
+                    container.classList.remove("loading")
+                    fragment.parentElement.remove()
+                })();
+            </script>
+        </div>
+    `
+}
 
 const randomWait = () =>
-  new Promise(resolve => setTimeout(resolve, Math.random() * 5000 + 100));
+    new Promise((resolve) => setTimeout(resolve, Math.random() * 5000 + 100))
 
 const deferredLoad = async (res, selector) => {
-  const script = await deferredHtml(selector, `handlers/home/${selector}.html`);
-  await randomWait();
-  res.write(script);
-  console.log(`wrote ${selector}`);
-};
+    const script = await deferredHtml(
+        selector,
+        `handlers/home/${selector}.html`
+    )
+    await randomWait()
+    res.write(script)
+    console.log(`wrote ${selector}`)
+}
 
 export const home = async (req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  const shell = await readFile("handlers/home/shell.html", {
-    encoding: "utf-8",
-  });
-  res.write(shell);
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    const shell = await readFile('handlers/home/shell.html', {
+        encoding: 'utf-8',
+    })
+    res.write(shell)
 
-  await Promise.all([
-    deferredLoad(res, "header"),
-    deferredLoad(res, "nav"),
-    deferredLoad(res, "aside"),
-    deferredLoad(res, "main"),
-    deferredLoad(res, "footer"),
-  ]);
-  res.end("\n");
-};
+    await Promise.all([
+        deferredLoad(res, 'header'),
+        deferredLoad(res, 'nav'),
+        deferredLoad(res, 'aside'),
+        deferredLoad(res, 'main'),
+        deferredLoad(res, 'footer'),
+    ])
+    res.end('\n')
+}
