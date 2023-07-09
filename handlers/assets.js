0764b2590f54106c9a55e39e5f875727c74a54ca
Andy VanEe <1andyvanee@gmail.com>
2023-07-08 19:24:28 -0600

    handlers

---
+++ handlers/assets.js
@@ -0,0 +1,38 @@
+import { dirname, join, extname } from "node:path";
+import { readFile, stat } from "node:fs/promises";
+const rootDir = join(dirname(new URL(import.meta.url).pathname), "assets");
+
+const mimeTypes = {
+  default: "application/octet-stream",
+  ".html": "text/html",
+  ".js": "text/javascript",
+  ".css": "text/css",
+  ".png": "image/png",
+  ".jpg": "image/jpeg",
+  ".gif": "image/gif",
+  ".svg": "image/svg+xml",
+  ".json": "application/json",
+  ".woff": "font/woff",
+  ".woff2": "font/woff2",
+  ".ttf": "font/ttf",
+  ".eot": "font/eot",
+  ".otf": "font/otf",
+  ".txt": "text/plain",
+  ".ico": "image/x-icon",
+};
+
+export const assets = async (req, res) => {
+  const { url } = req;
+  const filename = url.replace(/\/$/, "/index.html").slice(1);
+  const path = join(rootDir, filename);
+  try {
+    const fileinfo = await stat(path);
+    const ext = mimeTypes[extname(path)] || mimeTypes["default"];
+    if (fileinfo.isFile()) {
+      const content = await readFile(path);
+      res.writeHead(200, { "Content-Type": ext });
+      res.end(content);
+      return true;
+    }
+  } catch (error) {}
+};
---
+++ server.js
@@ -0,0 +1,14 @@
+import http from "node:http";
+import { home } from "./handlers/home.js";
+import { assets } from "./handlers/assets.js";
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(async (req, res) => {
+  if (await assets(req, res)) return;
+  home(req, res);
+});
+
+server.listen(PORT, () => {
+  console.log("Server listening", { PORT });
+});
